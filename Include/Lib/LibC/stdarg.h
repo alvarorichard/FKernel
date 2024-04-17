44c0203c3531e0ac6c246fc32094d4c3acfcbc61
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-04-17 16:04:08 -0300

    feat: adding a ffreestanding stdarg, stddef and stdint with compiler builtins
    Maybe can'be portable and we need make a manual implementation but now
    is ok run with __builtin__

---
+++ Include/Lib/LibC/stdarg.h
@@ -0,0 +1,14 @@
+/*
+* Stdarg.h
+* SPDX-License-Identifier: BSD-3-Clause
+* Copyright (c) 2024, FoxBSD
+*/
+
+#pragma once
+
+typedef __builtin_va_list va_list;
+
+#define va_start(ap, param) __builtin_va_start(ap, param)
+#define va_arg(ap, type)    __builtin_va_arg(ap, type)
+#define va_end(ap)          __builtin_va_end(ap)
+#define va_copy(dest, src)  __builtin_va_copy(dest, src)

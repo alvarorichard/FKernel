9c08a6037a99613ca61ccc26288c47a435ebe735
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-04-18 02:10:02 -0300

    feat: this is a custom header to englobe all Kernel/Io/

---
+++ Include/Kernel/Misc/cpu_func.hpp
@@ -0,0 +1,12 @@
+/*
+ * Cpu_func.h
+ * SPDX-Identifier-License: BSD-3-Clause
+ * Copyright (c) 2024 FoxBSD
+ */
+
+#pragma once
+
+#include <Kernel/Io/farpeek_poke.hpp>
+#include <Kernel/Io/io_operations.hpp>
+#include <Kernel/Io/cpu_operations.hpp>
+
